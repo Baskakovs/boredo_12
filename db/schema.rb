b4d0606aea002101ef02f1d3d08ca25ffb488983
Baskakovs <baskakovs@me.com>
2023-06-14 01:04:28 +0300

    published column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_210841) do
+ActiveRecord::Schema.define(version: 2023_06_13_220348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2023_06_13_210841) do
     t.integer "title_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "published"
   end
 
   create_table "subcomments", force: :cascade do |t|
