caa5d01d131801f9d4b27fd5432358e8fc91a861
Baskakovs <baskakovs@me.com>
2023-06-13 16:31:03 +0300

    last

--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_131424) do
+ActiveRecord::Schema.define(version: 2023_06_13_131418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "geography_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "text"
-    t.integer "post_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "geographies", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "text"
-    t.integer "category_id"
-    t.integer "geography_id"
-    t.integer "title_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "subcomments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "comment_id"
-    t.integer "post_id"
-    t.string "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "titles", force: :cascade do |t|
-    t.string "name"
-    t.integer "geography_id"
-    t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.date "date_of_birth"
-    t.string "password_digest"
-    t.boolean "google"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
