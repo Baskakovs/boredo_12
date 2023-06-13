e971389c0943f8f6c2d67602ef22db64f0984a35
Baskakovs <baskakovs@me.com>
2023-06-14 00:44:42 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
 require 'faker'
 
 geographies = ["USA", "UK", "Germany", "China", "France", "Japan", "EU", "Poland", "Ukraine", "Brazil", "Canada","India", "Russia", "Australia", "Mexico", "South Korea", "Italy", "Spain", "Netherlands", "Switzerland", "Sweden", "Norway", "Argentina", "Turkey", "Saudi Arabia", "United Arab Emirates", "South Africa", "Egypt", "Indonesia", "Singapore", "Thailand", "New Zealand", "Greece", "Belgium", "Austria", "Portugal"]
@@ -8,6 +16,9 @@ puts "Seeding the database..."
 
 geographies = Geography.all
 
+geographies.each do |geography|
+    Geography.create(name: geography)
+end
 
 categories = Category.all
 
@@ -22,3 +33,5 @@ titles = Title.all
 titles.each do |title|
   Post.create(text: Faker::Lorem.paragraph(sentence_count: 3), title_id: title.id, geography_id: title.geography_id, category_id: title.category_id, user_id: User.all.sample.id)
 end
+
+puts "Done seeding!"
